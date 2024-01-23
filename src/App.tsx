9184923a74e06c22b5a5d509fc994c1677c6b6a6
Martin Hubelbauer <joeysykes18@gmail.com>
2024-01-23 17:17:37 +0100

    Add logic to display current ISO date and time
    Co-developed using ChatGPT

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <>
-      <h1>Current Date is</h1>
-    </>
-  );
+class App extends React.Component {
+  timerID!: NodeJS.Timeout;
+
+  state = {
+    currentDateTime: new Date().toISOString(),
+  };
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      currentDateTime: new Date().toISOString(),
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>⏰</h1>
+          <p>Current date and time (ISO format) is: {this.state.currentDateTime}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
