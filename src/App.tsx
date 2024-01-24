bbf96fd73e478eac302ae88c3c81173c1025211d
Martin Hubelbauer <joeysykes18@gmail.com>
2024-01-24 11:53:52 +0100

    Style date and time

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [currentDateTime, setCurrentDateTime] = useState(new Date().toISOString());
+  const [date, setDate] = useState<string>();
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
 
   useEffect(() => {
     const timerID = setInterval(() => {
-      setCurrentDateTime(new Date().toISOString());
+      const now = new Date();
+      setDate(now.toDateString());
+      setHour(now.getHours());
+      setMinute(now.getMinutes());
+      setSecond(now.getSeconds());
     }, 1000);
 
     return () => clearInterval(timerID);
@@ -16,7 +23,14 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>⏰</h1>
-        <p>Current date and time (ISO format) is: {currentDateTime}</p>
+        <p className="date">{date}</p>
+        <div className="time">
+          <span className="hour">{hour?.toString().padStart(2, '0')}</span>
+          <span className="colonhm colon">:</span>
+          <span className="minute">{minute?.toString().padStart(2, '0')}</span>
+          <span className="colonms colon">:</span>
+          <span className="second">{second?.toString().padStart(2, '0')}</span>
+        </div>
       </header>
     </div>
   );
